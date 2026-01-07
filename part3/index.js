7b55dae668f439794ae866b925108fc265159d90
Beckham <beckhamle2023@gmail.com>
2026-01-06 23:16:41 -0700

    Finished node.js and express reading section for part3

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
 const express = require('express'); // import express which is a function
 const app = express(); // create an express app and store it in variable app
 
+app.use(express.json()) // activates json-parser to access data easily
+
 let notes = [
   {
     id: '1',
@@ -19,11 +21,12 @@ let notes = [
   },
 ];
 
+// HANDLES ALL HTTP REQUESTS FOR GETTING MAIN URL FOR SERVER
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
 });
 
-// HANDLES ALL HTTP REQUESTS GETTING ALL NOTES
+// HANDLES ALL HTTP REQUESTS FOR GETTING ALL NOTES
 app.get('/api/notes', (request, response) => {
   response.json(notes);
 });
@@ -48,6 +51,40 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end(); // return no data with response if note is found and deleted successfully
 });
+
+// _________________________________________________________________
+
+//Function to generate unique Id for new note
+const generateId = () => {
+  // find out largest id number in current list and assign it to maxId var
+  const maxId = notes.length > 0  // conditional to check if list is empty or not
+  ? Math.max(...notes.map(n => Number(n.id))) // if not empty use max method to find largest id number in list
+  : 0 // if current list is empty set id number to 0
+
+  return String(maxId + 1)  // return new id to be maxId+1
+}
+
+// HANDLES ALL REQUESTS FOR ADDING A NOTE TO SERVER
+app.post('/api/notes', (request, response) => {
+  const body = request.body 
+  
+  // if received data is missing value in content property -> server sends back 400 status code of a bad request made
+  if(!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false, // if important property is missing in request then default to setting it to false
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
 
 const PORT = 3001;
 app.listen(PORT);
