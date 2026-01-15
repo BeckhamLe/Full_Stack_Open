d8c2ed27d070a3243f08ec1942444d04fef49a59
Beckham <beckhamle2023@gmail.com>
2026-01-14 17:53:50 -0700

    Finished main functionality of Practice 2

--- practice/p2/p2_client/src/App.jsx
+++ practice/p2/p2_client/src/App.jsx
@@ -5,12 +5,74 @@ import Student from './components/Student.jsx'
 const App = () => {
   const [students, setStudents] = useState([])
 
+  const [newName, setNewName] = useState('...new student name')
+  const [newAge, setNewAge] = useState('...new student age')
+
+  const [deleteName, setDeleteName] = useState('...name of student to delete')
+
   useEffect(() => {
     studentServices.getAll().then((initialStudents) => {
       setStudents(initialStudents)
     })
     console.log("Imported initial students")
   }, [])
+
+  // ------------------------------------------- ADDING NEW STUDENT FUNCTIONALITY -----------------------------------------------
+
+  // ADDING NEW STUDENT FUNCTION
+  const addNewStudent = (event) => {
+    event.preventDefault(); //prevents the default action of submission and reloading the page
+
+    const newStudent = {
+      name: newName,
+      age: newAge
+    }
+      
+    studentServices.create(newStudent).then((returnedStudent) => {
+      setStudents(students.concat(returnedStudent))
+      setNewName('')
+      setNewAge('')
+    })
+    
+  }
+
+  // EVENT HANDLER TO UPDATE THE NEW NAME INPUTTED
+  const handleNewNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  // EVENT HANDLER TO UPDATE THE NEW AGE INPUTTED
+  const handleNewAgeChange = (event) => {
+    console.log(event.target.value)
+    setNewAge(event.target.value)
+  }
+
+// ------------------------------------------------------ DELETING STUDENT FUNCTIONALITY ----------------------------------------------------------------
+
+// DELETING A STUDENT FUNCTION
+const deleteStudent = (event) => {
+  event.preventDefault()
+
+  const deleteStudent = students.find((student) => student.name === deleteName)
+  studentServices.remove(deleteStudent.id).then((status) => {
+    if(status === 204) {
+      console.log("Student Successfully deleted")
+      setStudents(students.filter((student) => student.name != deleteName))
+      setDeleteName('')
+    }
+  })
+  .catch((err) => {
+    console.log('Failed to delete: ', err.response?.status)
+  })
+
+}
+
+// EVENT HANDLER TO UPDATE NAME OF STUDENT BEING DELETED
+const handleDeleteNameChange = (event) => {
+  console.log(event.target.value)
+  setDeleteName(event.target.value)
+}
 
   return (
     <>
@@ -20,6 +82,23 @@ const App = () => {
             <Student key={student.id} name={student.name} age={student.age} />
           ))}
         </ul>
+      </div>
+      <div>
+        <form onSubmit={addNewStudent}>
+          <input value={newName} onChange={handleNewNameChange} />
+          <br/>
+          <input value={newAge} onChange={handleNewAgeChange} />
+          <br/>
+          <button type='submit'>Add Student</button>
+        </form>
+      </div>
+      <br/>
+      <div>
+        <form onSubmit={deleteStudent}>
+          <input value={deleteName} onChange={handleDeleteNameChange} />
+          <br/>
+          <button type='submit'>Delete</button>
+        </form>
       </div>
     </>
   )
--- practice/p2/p2_client/src/services/students.js
+++ practice/p2/p2_client/src/services/students.js
@@ -6,4 +6,14 @@ const getAll = () => {
     return request.then((response) => response.data)
 }
 
-export default { getAll }
+const create = (newStudent) => {
+    const request = axios.post(baseUrl, newStudent)
+    return request.then((response) => response.data)
+}
+
+const remove = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then((response) => response.status)
+}
+
+export default { getAll, create, remove }
